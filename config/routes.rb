cb079c52b68e83baeff77637075bd61e52383833
PaulHowley <paulhowley1976@googlemail.com>
2019-01-30 16:55:18 +0000

    Set up the basic functionality of the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+ resources :articles do
+  resources :comments
+ end
+
+  namespace :admin do
+    resources :articles, :comments
+  end
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
