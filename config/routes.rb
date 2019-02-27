d0132fe5012de0adfa9bb0de83b58c44e6c52d89
PaulHowley <paulhowley1976@googlemail.com>
2019-02-27 16:39:24 +0000

    Style static and articles pages, applying BEM namespacing and setting up component stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'home', to: 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
+  get 'projects', to: 'static_pages#projects'
+
   resources :articles, only: [:index, :show] do
     # resources :comments
   end
