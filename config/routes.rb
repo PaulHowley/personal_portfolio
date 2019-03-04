171619c8fc32cc21737db9cc930f2de21c7cc34f
PaulHowley <paulhowley1976@googlemail.com>
2019-03-04 15:11:14 +0000

    streamline spec files and update disqus spec to ensure disqus_thread showing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
-  get 'home', to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'projects', to: 'static_pages#projects'
