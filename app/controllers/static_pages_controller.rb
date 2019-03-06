b8fea9a95abd9f5e652b4f9ccdfd175a1fa48fc5
PaulHowley <paulhowley1976@googlemail.com>
2019-03-06 16:47:16 +0000

    use a view helper to determine background based on the the request url, rather than using the controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @home_page = true
   end
 
   def about
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def static_bg(action)
+    if action == 'home'
+      "c-home__bg"
+    else
+      "c-container"
+    end
+  end
 end
