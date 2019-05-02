06a40fbec7d9fa1cdbeb746df57e8e61fb894e86
PaulHowley <paulhowley1976@googlemail.com>
2019-05-02 17:16:06 +0100

    finish Rspec test for adding a comment

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-Rspec.describe "comments", type: :feature do
+RSpec.describe "comments", type: :feature do
   describe "Adding a comment" do
     let(:article) { create(:article) }
 
     it "should add a comment" do
       visit article_path(article)
 
-      fill_in "commenter", with: "Commenter name"
-      fill_in "body", with: "Commenter text"
+      fill_in("comment[commenter]", with: "Commenter name")
+      fill_in("comment[body]", with: "Commenter text")
       click_button "Submit"
 
-      expect(page).to have_content "Commenter name"
-      expect(page).to have_content "Commenter text"
+      expect(page).to have_content("Commenter name")
+      expect(page).to have_content("Commenter text")
     end
   end
+end
 
   
